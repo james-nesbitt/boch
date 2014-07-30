386264946828dee1c0dfd434f30d760a1e8793cd
James Nesbitt <james.nesbitt@wunderkraut.com>
2014-07-30 20:25:07 +0300

    Massively cleaned up help, added run command, removed help command (it is replaced)

--- hooks/settings/030_default_settings.sh
+++ hooks/settings/030_default_settings.sh
@@ -28,32 +28,99 @@ used, and what settings to pass to the docker abstraction.
 # execute method
 hook_settings_030_execute()
 {
+  # Overrideable configuration variables
 
-  # General project name, should be file-safe
-  Project_name="${Project_name:-${path_project##*/}}"
+  ####
+  # Important paths for the source code
+  #
 
-  # Docker build images
+  # =path to custom settings and data=
+  # various settings about custom configurations
+  #
+  # - settings.sh : this file
+  #
+  #path_data="\${path_project}/data"
+
+  # path to the log file
+  # @NOTE to enable logging try passing --log [{level}] to you command line
+  #path_log="\${path_log}"
+
+  ###
+  # Project settings
+  #
+  # some project related settings
+  #
+
+  # Project name : this gets used as a default all over the place
+  project_name=\"${project_name:-"project"}\"
+
+  ###
+  # Docker image configurations
+  #
+
+  # Required build images
+  #
   # Generally needed to start making containers (a space separated queue of builds)
-  # and are the default builds built with the build command.
-  Docker_builds="${Docker_builds:-"wwwserver-cnpm wwwserver-cnpm-dev project"}"
-  # The docker image, which will be used for containers, (and building)
-  Docker_image="${Docker_image:-${Docker_builds##* }}"
-  # by default all containers will run from the base image, which is created by build, however maybe we want to use a different version for advanced features
-  Docker_imageversion="${Docker_imageversion:-"latest"}" # Used for run/shell/commit
-  # use a default container name, default to a combination of image and version
-  Docker_container=${Docker_container:-${Docker_image}_${Docker_imageversion}}
+  # and are the default docker builds built with the flow init command.
+  # Change this if your build image has a different name, or if it has different
+  # parent images that need building.
+  #required_builds="${required_builds:-""}"
 
+  # Project build and image
+  #
+  # which build (folder) should be used for project images (if it isn't built)
+  project_build="${project_build:-"project"}"
+  # which image should we use for creating containers (and build it if it is missing)
+  project_image="${project_image:-"project"}"
+  # which image version/tag should we use for containers
+  project_imageversion=\"${project_imageversion:-"latest"}\"
 
-  # hostname used inside the running container
-  Machine_hostname="${Machine_hostname:-$Docker_container}"
+  ###
+  # Docker Container configurations
+  #
 
-  # Prefered shell/command for the Machine : bash is pretty safe, but we probably have zsh installed.  the custom settings defaults include zsh.
-  Machine_shell=${Machine_shell:-"/bin/bash"}
+  # OS hostname used inside the container (which would impact things like avahi)
+  machine_hostname="${Project_name}"
 
-  # Default running args
-  Machine_runargs=${Machine_runargs:-""}
-  # default run args for shell command
-  Machine_shellrunargs=${Machine_shellrunargs:-"--publish-all=true"}
+  # Machine arguments for regular container runs.  These arguments are added to all runs (except shell runs)
+  # @NOTE these are arguments for \"docker run\". Check that docker command to learn more
+  machine_runargs=\"--tty\" # --tty is needed for supervisord to run.
+
+  ###
+  # Prefered shell configuration for the Machine
+  #
+  # Running as a shell is a temporary approach to getting shell access to a machine
+  # but should be considered secondary to other methods of running commands on the
+  # container.  It is a good plan for now but will be dropped in the future.
+  #
+  # @NOTE Running a shell creates a new container, but does not give access to an existing container
+  #
+  # @NOTE if you are using our builds, then zsh is available
+  machine_shell="/bin/bash"
+  # arguments for docker run that should always be included when starting a shell: docker help run
+  # @NOTE you should probably take a look at docker run to learn more
+  # @NOTE our builds have a developer user created
+  machine_shellrunargs="--publish-all=true"
+
+  ###
+  # Mount list:
+  #
+  # These are run time mounts, where the host FS can be changed
+  # directly changing the container FS.
+  #
+  # @TODO get away from having to include the -v flag here, by finding a better format for this
+
+  # create some variables for our sources
+  # @NOTE these folders assume we are using the default www root, and nginx conf.  Change this if you want to customize.
+  path_source="${path_project}/source"
+  #path_source_www="${path_source}/www"
+
+  # Now that we have tested it, add the source folder to the mount
+  # This is an example from what is already added by the system == Machine_mountvolumes=\"\${Machine_mountvolumes} --volume=\${path_project}/source:/app/source\"
+
+  # Mount the external user .ssh folder in the developer user home directory, so it can be used for stuff
+  # @NOTE our builds have a developer user created
+  #Machine_mountvolumes="${Machine_mountvolumes} --volume=${path_userhome}/.ssh:/home/developer/.ssh-host"
 
   # live mounts that are used for containers
   path_source="${path_project}/source"
@@ -61,7 +128,7 @@ hook_settings_030_execute()
 
   if [ $? == 0 ]; then
     debug --level 8 --topic "HOOK->SETTINGS->030" "Created required folder for project source code, adding it to the mount path list: $path_source"
-    Machine_mountvolumes="${Machine_mountvolumes} --volume=${path_source}:/app/source"
+    machine_mountvolumes="${machine_mountvolumes} --volume=${path_source}:/app/source"
   else
     debug --level 3 --topic "HOOK->SETTINGS->030" "Failed to create required folder for project source code path: $path_source"
   fi
